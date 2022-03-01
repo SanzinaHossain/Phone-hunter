719a06821f44755162c7818142a8034708a11ff9
SanzinaHossain <sanzina67hossain@gmail.com>
2022-03-02 00:36:34 +0600

    fifth

--- js/app.js
+++ js/app.js
@@ -36,23 +36,55 @@ const searchPhone=()=>
 const PhoneShow=(Phones)=>{
     clean();
     console.log(Phones);
-        
-        for(const phone of Phones.slice(0,20))
+    for(const phone of Phones.slice(0,20))
         {
             const mobileShow=document.getElementById('data-show');
             const div=document.createElement('div');
             div.classList.add('col');
             div.innerHTML=`
-               <div style="background-color:wheat;" class="card h-100">
-                  <img src="${phone.image}" class="card-img-top" alt="...">
+               <div style="background-color:wheat;" class="card h-100 p-3">
+                  <img src="${phone.image}" style="height:400px;width:300px;" class="card-img-top img-fluid" alt="...">
                 <div style="background-color:wheat;"class="card-body">
-                   <h3 class="card-title">${phone.phone_name}</h3>
-                   <h5 class="card-title">Brand: ${phone.brand}</h5>
+                   <h3 class="card-title text-center">${phone.phone_name}</h3>
+                   <h5 class="card-title text-center">Brand: ${phone.brand}</h5>
                 </div>
-                 <button "style="font-weight:700;background-color:yellowgreen;"class=" rounded bg-warning">Details</button>
+                 <button onclick="ShowPhoneDetails('${phone.slug}') "style="font-weight:700;background-color:yellowgreen;"class=" rounded bg-warning">Details</button>
                </div>
             `;
             mobileShow.appendChild(div);
 
         }
+}
+const ShowPhoneDetails=(phoneid)=>{
+    const url=`https://openapi.programming-hero.com/api/phone/${phoneid}`;
+     fetch(url)
+      .then(res=>res.json())
+      .then(data=>GoforDetails(data.data));
+}
+const GoforDetails=(data)=>{
+    let res=data.releaseDate;
+    if(res=='')
+    {
+        res=' Not found';
+    }
+    const phoneD=document.getElementById('phone-details');
+    phoneD.textContent='';
+    const div=document.createElement('div');
+    div.classList.add('card','card-details','m-3','mx-auto','w-50');
+    div.innerHTML=`
+       <img src="${data.image}" class="card-img-top img-fluid p-3 p-lg-5" alt="...">
+       <div class="card-body">
+          <h2 class="card-title text-center text-warning ">${data.name}</h2>
+          <h4 class="text-center">Brand: ${data.brand}</h4>
+        </div>
+       <div class="card-body">
+       <ul class="list-group list-group-flush">
+            <li class="list-group-item">Release Date: ${res}</li>
+           <li class="list-group-item">A second item</li>
+           <li class="list-group-item">A third item</li>
+           <li class="list-group-item">A third item</li>
+        </ul>
+       </div>
+    `
+    phoneD.appendChild(div);
 }
